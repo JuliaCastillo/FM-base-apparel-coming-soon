07007286bb6dbc67e60d2c00e3796af28c63a574
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-07-21 17:25:00 +0200

    Finished email validation

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ function validateEmail(email) {
 
 const inpgroup = document.getElementById("inp-group");
 const textinput = document.getElementById("email");
+const warningElements = document.querySelectorAll(".warning");
 
 function validation() {
   validEmail = validateEmail(textinput.value);
 
   if (!validEmail) {
     inpgroup.classList.add("not-valid");
+    for (let i = 0; i < 2; i++) {
+      warningElements[i].style.display = "block";
+    }
+    // warningElements.style.display = "block";
   } else {
     inpgroup.classList.remove("not-valid");
+    for (let i = 0; i < 2; i++) {
+      warningElements[i].style.display = "none";
+    }
   }
 }
